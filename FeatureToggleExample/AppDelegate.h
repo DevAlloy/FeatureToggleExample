abecbb18a5c7f1e123c27c701fa904832ff20bf8
Tsyganov Stanislav <s.tsyganov@rambler-co.ru>
2016-07-19 00:05:20 +0300

    Начальный коммит

---
+++ FeatureToggleExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FeatureToggleExample
+//
+//  Created by Tsyganov Stanislav on 19/07/16.
+//  Copyright © 2016 DevAlloy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FeatureToggleExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FeatureToggleExample
+//
+//  Created by Tsyganov Stanislav on 19/07/16.
+//  Copyright © 2016 DevAlloy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
